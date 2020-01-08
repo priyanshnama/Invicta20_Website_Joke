83b60e88a95d2f86f6fda822f9a868ce965c1ebb
Priyansh Nama <54625644+priyanshnama@users.noreply.github.com>
2020-01-08 19:51:28 +0530

    Update code.py

--- code.py
+++ code.py
@@ -2,27 +2,31 @@
 # root login password Tendua.jr123
 # root Refral Code Tendua54
 import urllib
-import urllib2
-import requests
 import string 
 import random 
 import time
-from bs4 import BeautifulSoup
 
-# getting temp email
-res = ''.join(random.choices(string.ascii_uppercase + string.digits, k = N))
+#fields
+firstname = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 5))
+email = firstname + "@gmail.com"
+lastname = 'Jr'
+college = 'IIITDMJ'
+city = 'Dumna'
+phone = '1234567890'
+password = 'dumnarockzz'
+refral = 'Tendua54'
 
 
 data = {
-        "firstname" : firstname
-        "lastname"  : lastname
-        "email"     : email
-        "college"   : college
-        "city"      : city
-        "phone"     : phone
-        "password"  : password
-        "refral"    : refral
+        "firstname" : firstname,
+        "lastname"  : lastname,
+        "email"     : email,
+        "college"   : college,
+        "city"      : city,
+        "phone"     : phone,
+        "password"  : password,
+        "refral"    : refral,
        }
-encoded_data = urllib.urlencode(data)
-content = urllib2.urlopen("https://invicta20.org/register?action=validate(event)",
-        encoded_data)
+       
+#encoded_data = urllib.urlencode(data)
+#content = urllib2.urlopen("https://invicta20.org/register?action=validate(event)",encoded_data)
